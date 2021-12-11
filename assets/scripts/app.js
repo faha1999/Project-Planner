e3a762525ed460cba096628313c6ade29833bae4
faha1999 <kafahad1999@gmail.com>
2021-12-11 17:09:34 +0600

    applied shitch logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,39 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click');
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of projItems) {
+      this.projects.push(new ProjectItem(projItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.addProject();
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
